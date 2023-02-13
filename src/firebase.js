a2d6cb25518f0a8201262a67051bf8428978561d
Ivan Yang <yangivan0614@gmail.com>
2023-02-13 15:47:53 -0500

    Boiler plate react code

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmN_FaKypjOKQQFrR91ClS76B0YG8bNZ0",
+  authDomain: "stakethestigma-dc208.firebaseapp.com",
+  projectId: "stakethestigma-dc208",
+  storageBucket: "stakethestigma-dc208.appspot.com",
+  messagingSenderId: "748785801153",
+  appId: "1:748785801153:web:b5181a696b692c3f024140",
+  measurementId: "G-H1E8HTJJJ4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
