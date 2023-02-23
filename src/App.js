14ff9d7d0a4e154dc6af681fe60d6627faabb1c2
Ivan Yang <yangivan0614@gmail.com>
2023-02-22 23:45:35 -0500

    Added handler to add GuestUsers to database

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <Route path="/" element = {<Login />} />
       </Routes>
     </Router>
-  
   );
 }
 
--- src/components/scripts/login.js
+++ src/components/scripts/login.js
@@ -1,12 +1,23 @@
 import styles from "../styles/Login.module.css";
 import { db } from "../../firebase";
+import { doc, setDoc, collection, addDoc } from "firebase/firestore"; 
+
 
 function Login() {
+
+    const addUserHandler = () => {
+        addDoc(collection(db, "guestUsers"), {
+            name: "idk"
+        });
+        alert("Guest user added");
+    }
+    
+
     return (
         <main>
-            <logo>Stake The Stigma</logo>
+            <h1 className = {styles.logo}>Stake The Stigma</h1>
             <nav>
-             <button className={styles.guest}>Guest User</button>
+             <button className={styles.guest} onClick = {addUserHandler}>Guest User</button>
              <button className={styles.login}>Log in</button>
              <button className={styles.contributor}>Contributor</button>
         </nav>
