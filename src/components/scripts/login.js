a155089627e109d4d8e23ace4a084ee4b15a8ab6
Ivan Yang <yangivan0614@gmail.com>
2023-03-11 18:15:24 -0500

    Added support for anonymous/guest users

--- src/components/scripts/login.js
+++ src/components/scripts/login.js
@@ -2,22 +2,33 @@ import styles from "../styles/Login.module.css";
 import { db } from "../../firebase";
 import { doc, setDoc, collection, addDoc } from "firebase/firestore"; 
 import { Link } from "react-router-dom";
-import { auth } from "../../firebase";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
 
-    const addUserHandler = () => {
-        addDoc(collection(db, "guestUsers"), {
-            name: "idk"
-        });
-        alert("Guest user added");
+    const navigate = useNavigate();
+    
+    const addGuestUserHandler = () => {
+        const auth = getAuth();
+        signInAnonymously(auth)
+            .then(auth => {
+                console.log("Guest signed in")
+                // Signed in..
+                navigate("/survey");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+
+            });
     }
     
     return (
         <main>
             <h1 className = {styles.logo}>Stake The Stigma</h1>
             <nav>
-                <button className={styles.guest} onClick = {addUserHandler}>Guest User</button>
+                <button className={styles.guest} onClick = {addGuestUserHandler}>Guest User</button>
                 <button>
                     <Link to = {"/register"} style={{ textDecoration: 'none' }}>
                         Register/Login
