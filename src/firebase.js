a212c937c50ecd63a46eaeb20f19105381eb6757
Ivan Yang <yangivan0614@gmail.com>
2023-03-26 17:01:29 -0400

    Removed Summarization.js/cloud functions

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +21,6 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const functions = getFunctions(app);
+
 
 
