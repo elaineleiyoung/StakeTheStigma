8fa8293bd13dd54d51562473339f6430e461543c
Geneva Yang <yuhanyang@nat-wireless-guest-reg-153-54.bu.edu>
2023-03-27 00:35:53 -0400

    pushing ui changes! :)

--- src/components/scripts/register.js
+++ src/components/scripts/register.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Register.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, getUserByEmail } from "firebase/auth";
 
 function Register() {
@@ -15,7 +16,7 @@ function Register() {
         const auth = getAuth();
         signInWithEmailAndPassword(auth, email, password)
         .then(auth => {
-            navigate("/survey");
+            navigate("/dashboard");
         })
         .catch(error => alert(error.message))
     }
@@ -27,7 +28,7 @@ function Register() {
         createUserWithEmailAndPassword(auth, email, password)
         .then((auth) => {
             if (auth) {
-                navigate("/survey");
+                navigate("/");
             }
         })
         .catch(error => alert(error.message))
@@ -36,7 +37,7 @@ function Register() {
     return (
         <div className = {styles.sheesh}>
              <h1 className = {styles.logo}>STAKE THE STIGMA.</h1>
-             <h2 className={styles.logo2}>_UNCENSOR CENSORED NEWS_</h2>
+             <h2 className={styles.logo2}>_UNCENSORING CENSORED NEWS_</h2>
         
         <section className = {styles.layout}>
             <div style={{ display: "flex", justifyContent: "center", marginTop: "25vh", marginLeft: "100px" }}>
@@ -50,7 +51,11 @@ function Register() {
                         <input type = "password" value = {password} onChange = {e => {setPassword(e.target.value)}} className = {styles.password} />
                     </div>
 
-                    <button type = "submit" onClick = {signIn} className = {styles.signInBtn}>Sign In</button>
+                    <button type = "submit" onClick = {signIn} className = {styles.signInBtn}>
+                        <Link to = {"/dashboard"} color= "inherit" style={{textDecoration:'none'}}>
+                            Sign In
+                        </Link>
+                    </button>
                 </form>
 
                 <p className = {styles.message}>Don't have an account? Create one here!</p>
